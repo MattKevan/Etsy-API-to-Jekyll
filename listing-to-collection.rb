6f189296ee9d6a5ef8a32e42c634c899a4800210
Matt Kevan <m@kevan.tv>
2021-06-13 10:22:43 +0100

    Update to latest version

--- listing-to-collection.rb
+++ listing-to-collection.rb
@@ -1,12 +1,14 @@
 require 'net/http'
+require 'open-uri'
 require 'json'
- 
+
 apiKey = 'YOUR_API_KEY'
 shopID = 'YOUR_SHOP_ID'
-rateLimit = '120'
+offset = ARGV[0]
 stopChar = '|'
 
-listing_url = "https://openapi.etsy.com/v2/shops/#{shopID}/listings/active?method=GET&api_key=#{apiKey}&limit=#{rateLimit}"
+
+listing_url = "https://openapi.etsy.com/v2/shops/#{shopID}/listings/active?method=GET&api_key=#{apiKey}&limit=100&offset=#{offset}"
 listing_uri = URI(listing_url)
 
 listing_response = Net::HTTP.get(listing_uri)
@@ -18,19 +20,18 @@ listing_info = listing_parsed["results"].each do |listing|
 	shortTitle = fullTitle.split("#{stopChar}")[0]
 	slug = shortTitle.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 
-	File.open("#{slug}.md", 'w') do |file|
+	File.open("../_artwork/#{slug}.md", 'w') do |file|
 
 		file.puts "---"
-		file.puts "layout: product"
-		file.puts "title: #{shortTitle}"
-		file.puts "full-title: #{fullTitle}"
+		file.puts "title: '#{shortTitle}'"
+		file.puts "full-title: '#{fullTitle}'"
 		file.puts "listing_id: #{listing["listing_id"]}"
 		file.puts "etsy_url: #{listing["url"]}"
 		file.puts "section_id: #{listing["shop_section_id"]}"
 		file.puts "price: #{listing["price"]}"
 		file.puts "tags: #{listing["tags"]}"
 
-		image_url = "https://openapi.etsy.com/v2/listings/#{listing["listing_id"]}/images?method=GET&api_key=#{apiKey}&limit=#{rateLimit}"
+		image_url = "https://openapi.etsy.com/v2/listings/#{listing["listing_id"]}/images?method=GET&api_key=#{apiKey}&rateLimit=100&offset=#{offset}"
 		image_uri = URI(image_url)
 
 		image_response = Net::HTTP.get(image_uri)
@@ -38,9 +39,29 @@ listing_info = listing_parsed["results"].each do |listing|
 
 		count = 0
 		image_parsed["results"].each do |image|
-			file.puts "image_75x75_url_#{count}: #{image["url_75x75"]}"
-			file.puts "image_570xN_url_#{count}: #{image["url_570xN"]}"
-			file.puts "image_fullxfull_url_#{count}: #{image["url_fullxfull"]}"
+
+			open("#{image["url_fullxfull"]}") do |image|
+			  File.open("../images/artwork/#{listing["listing_id"]}_full_#{count}.jpg", "wb") do |file|
+			    file.write(image.read)
+			  end
+			end
+
+			open("#{image["url_570xN"]}") do |image|
+			  File.open("../images/artwork/#{listing["listing_id"]}_medium_#{count}.jpg", "wb") do |file|
+			    file.write(image.read)
+			  end
+			end
+
+			open("#{image["url_75x75"]}") do |image|
+			  File.open("../images/artwork/#{listing["listing_id"]}_thumbnail_#{count}.jpg", "wb") do |file|
+			    file.write(image.read)
+			  end
+			end
+
+
+			file.puts "image_thumbnail_#{count}: #{listing["listing_id"]}_thumbnail_#{count}.jpg"
+			file.puts "image_medium_#{count}: #{listing["listing_id"]}_medium_#{count}.jpg"
+			file.puts "image_full_#{count}: #{listing["listing_id"]}_full_#{count}.jpg"
 			count = count+1
 		end
 
@@ -49,4 +70,5 @@ listing_info = listing_parsed["results"].each do |listing|
 		file.puts "#{listing["description"]}"
 
 	end
+	puts "#{slug}"
 end
